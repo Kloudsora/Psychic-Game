2d2775ea1aaa4445a2e83e99df56f126bfe1feaa
kol10 <koltonbingham@gmail.com>
2018-11-17 14:19:49 -0700

    afterclass

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -8,59 +8,66 @@ var alreadyguess = [""];
 
 var pcopt = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-var actual =(function getrandomletter() 
- {
-     return pcopt[Math.floor(Math.random() * pcopt.length)];
- })() 
+var actual;
+
+function getrandomletter() {
+    actual = pcopt[Math.floor(Math.random() * pcopt.length)];
+}
+
+getrandomletter();
+
+// var actual = (function getrandomletter() {
+//     return pcopt[Math.floor(Math.random() * pcopt.length)];
+// })()
 
 var guessleft = 9;
 
 console.log(actual);
 
-    document.onkeyup = function(event)              
-    {
+var hopethisworks = document.onkeyup = function (event) {
 
-        let input = event.key;
+    var input = event.key;
 
-        var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-        
-        if (options.indexOf(input) > -1) 
-        
-        {
-            
-            if (input === actual) {
-                wins++;
-                guessleft = 9;
-                alreadyguess = [];
-                getrandomletter();
-                
-            }
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-            if (input != actual) {
-                guessleft --;
-                alreadyguess.push(input);
-            }
+    if (options.indexOf(input) > -1) {
 
-            if (guessleft === 0) {
+        if (input === actual) {
+            wins++;
+            guessleft = 9;
+            alreadyguess = [];
+            getrandomletter();
+            console.log(actual);
+        }
+
+        if (input != actual) {
+            guessleft--;
+            alreadyguess.push(input);
+        }
+
+        if (guessleft === 0) {
 
             guessleft = 9;
-            losses ++;
+            losses++;
             alreadyguess = [];
             getrandomletter();
 
+
+            return hopethisworks
+            return options
         }
-        
+
 
     }
-         
-        console.log(["input", input]) 
-        console.log(["guessleft", guessleft])
-        console.log(["wins", wins])
+
+    // console.log(["input", input])
+    // console.log(["guessleft", guessleft])
+    // console.log(["wins", wins])
 
 
-            document.getElementById("thewins").innerHTML = "Wins:"+wins+"";
-            document.getElementById("thelose").innerHTML = "Loses:"+losses+"";
-            document.getElementById("sofar").innerHTML = "Your guesses so far:" +alreadyguess+ "";
-            document.getElementById("remaining").innerHTML = "Guesses Remaining:" +guessleft+ "";
+    document.getElementById("thewins").innerHTML = "Wins:" + wins + "";
+    document.getElementById("thelose").innerHTML = "Loses:" + losses + "";
+    document.getElementById("sofar").innerHTML = "Your guesses so far:" + alreadyguess + "";
+    document.getElementById("remaining").innerHTML = "Guesses Remaining:" + guessleft + "";
 
 }
